39f0768f1f0f75efab50ab6fa4832b87de5250a2
postrou <postroukhov12@gmail.com>
2024-02-29 15:27:51 +0400

    run_crr.py: starmap->map for v2 and v3

--- run_crr.py
+++ run_crr.py
@@ -649,7 +649,8 @@ if __name__ == '__main__':
             batch_size, 
             step_size_list
         )
-        pool.starmap(partial_crr_shift, clip_level_list)
+        # partial_crr_shift(clip_level_list[0])
+        pool.map(partial_crr_shift, clip_level_list)
 
     elif algorithm == 'crr_shift_3':
         assert args.cl_min is not None and args.cl_max is not None, \
@@ -670,7 +671,8 @@ if __name__ == '__main__':
             batch_size, 
             step_size_list
         )
-        pool.starmap(partial_crr_shift, clip_level_list)
+        # pool.starmap(partial_crr_shift, clip_level_list)
+        pool.map(partial_crr_shift, clip_level_list)
 
     elif algorithm == 'so':
         print('Single Reshuffling')
