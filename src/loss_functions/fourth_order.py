32065069190d2f0913ddc728906ba07fecf8a55b
Petr Ostroukhov <ostroukhov@phystech.edu>
2024-08-22 23:58:47 +0300

    fourth_order.py: self.x -> self.x_0

--- src/loss_functions/fourth_order.py
+++ src/loss_functions/fourth_order.py
@@ -8,7 +8,7 @@ from .loss_oracle import Oracle
 class FourthOrder(Oracle):
     def __init__(self, x, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.x = x
+        self.x_0 = x
         assert len(x.shape) == 1
         self.n = len(x)
         self.dim = 1
@@ -18,15 +18,15 @@ class FourthOrder(Oracle):
         self.f_opt = None
 
     def value(self, x):
-        y = np.mean([(x - x_0) ** 4 for x_0 in self.x])
+        y = np.mean([(x - x_0) ** 4 for x_0 in self.x_0])
         return y
 
     def partial_value(self, x, idx):
-        y = np.mean([(x - x_0) ** 4 for x_0 in self.x[idx]])
+        y = np.mean([(x - x_0) ** 4 for x_0 in self.x_0[idx]])
         return y
 
     def gradient(self, x):
-        grad = np.mean([4 * (x - x_0) ** 3 for x_0 in self.x])
+        grad = np.mean([4 * (x - x_0) ** 3 for x_0 in self.x_0])
         return grad
 
     def stochastic_gradient(
@@ -42,9 +42,9 @@ class FourthOrder(Oracle):
         else:
             batch_size = 1 if np.isscalar(idx) else len(idx)
         if np.isscalar(idx):
-            x_0_list = [self.x[idx]]
+            x_0_list = [self.x_0[idx]]
         else:
-            x_0_list = self.x[idx]
+            x_0_list = self.x_0[idx]
         stoch_grad = np.mean([4 * (x - x_0) ** 3 for x_0 in x_0_list])
         return stoch_grad
     
