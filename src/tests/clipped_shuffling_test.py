967199aad2854ead3bb7893be3ea58eb8a12ac3d
Petr Ostroukhov <ostroukhov@phystech.edu>
2024-08-30 15:07:15 +0300

    clipped_shuffling_test.py: removed commented piece

--- src/tests/clipped_shuffling_test.py
+++ src/tests/clipped_shuffling_test.py
@@ -7,50 +7,6 @@ from src.loss_functions import load_fourth_order_dataset
 
 
 class ClERRTestCase(unittest.TestCase):
-
-    # def test_trace_len(self):
-    #     n_epochs = 10
-    #     batch_size = 32
-    #     step_size = 0.001
-    #     clip_level = 10000
-
-    #     loss, x0, x_opt, n_seeds, stoch_it, _, _, _ = \
-    #         load_fourth_order_dataset(n_epochs, batch_size, save_results=False)
-
-    #     trace_len = stoch_it * 0.25
-    #     c_0 = 1 / (2 * step_size)
-    #     c_1 = c_0 / clip_level
-    #     inner_step_size = 0
-    #     use_g = False
-    #     clerr_2 = ClERR2(
-    #         c_0=c_0,
-    #         c_1=c_1,
-    #         inner_step_size=inner_step_size,
-    #         loss=loss, 
-    #         it_max=stoch_it, 
-    #         batch_size=batch_size, 
-    #         trace_len=trace_len,
-    #         n_seeds=2, 
-    #         f_tolerance=None,
-    #         use_g_in_outer_step=use_g
-    #     )
-    #     clerr_2_trace = clerr_2.run(x0=x0)
-    #     clerr_2_trace.convert_its_to_epochs(batch_size=batch_size)
-    #     clerr_2_trace.compute_loss_of_iterates()
-
-    #     for i, seed in enumerate(clerr_2_trace.loss_vals_all):
-    #         if i != len(clerr_2_trace.loss_vals_all) - 1:
-    #             self.assertEqual(len(clerr_2_trace.xs_all[seed]), trace_len + 1, \
-    #                 f'seed #{i}')
-    #             self.assertEqual(len(clerr_2_trace.loss_vals_all[seed]), trace_len + 1, \
-    #                 f'seed #{i}')
-    #             self.assertEqual(len(clerr_2_trace.its_all[seed]), trace_len + 1, \
-    #                 f'seed #{i}')
-    #             self.assertEqual(len(clerr_2_trace.grad_estimators_all[seed]), trace_len + 1, \
-    #                 f'seed #{i}')
-    #             self.assertEqual(len(clerr_2_trace.outer_step_sizes_all[seed]), trace_len + 1, \
-    #                 f'seed #{i}')
-
     def test_gradient_norm_clerr_2_full_batch_crr_full_batch(self):
         n_epochs = 100
         n_seeds = 2
