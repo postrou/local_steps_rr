88d29d780002f36cee625cbe0bca1fe59402b018
Petr Ostroukhov <ostroukhov@phystech.edu>
2024-09-16 18:30:10 +0300

    - removed old notebooks and some useless files - gitignore upd

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='opt_methods', 
-      version='0.1',
-      maintainer='Konstantin Mishchenko',
-      license='MIT',
-      packages=find_packages())
