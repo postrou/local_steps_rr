9a613cd658cf11bceeec6ef790b04acdf25520d7
Petr Ostroukhov <ostroukhov@phystech.edu>
2024-10-08 19:25:05 +0300

    - fixed ShuffleOnceSampler - added RandomReshufflingSampler - fixed tests for both samplers

--- src/optimizers_torch/random_reshuffling_sampler.py
+++ src/optimizers_torch/random_reshuffling_sampler.py
@@ -2,15 +2,17 @@ import torch
 import numpy as np
 
 
-class ShuffleOnceSampler(torch.utils.data.Sampler):
+class RandomReshufflingSampler(torch.utils.data.Sampler):
     def __init__(self, data_source):
         self.data_source = data_source
         self.num_samples = len(data_source)
         self.indices = np.arange(self.num_samples)
-        # Shuffle indices only once before running optimization
-        np.random.shuffle(self.indices)
 
     def __iter__(self):
+        # Shuffle indices at the start of the epoch
+        # np.random.seed(self.epoch)  # Optional: To make the shuffling deterministic per epoch
+        np.random.shuffle(self.indices)
+        
         # Return the shuffled indices sequentially
         # for i in range(0, self.num_samples, self.batch_size):
             # idx = self.indices[i : i + self.batch_size]
--- tests/shuffling_torch_test.py
+++ tests/shuffling_torch_test.py
@@ -3,17 +3,17 @@ from itertools import product
 
 import numpy as np
 
-from src.optimizers_torch.shuffle_once_sampler import ShuffleOnceSampler
+from src.optimizers_torch import RandomReshufflingSampler, ShuffleOnceSampler
 
 
-class ShufflingTorchTest(unittest.TestCase):
-    def test_shuffling(self):
+class RandomReshufflingTorchTest(unittest.TestCase):
+    def test_random_shuffling(self):
         data = np.arange(10)
-        sampler = ShuffleOnceSampler(data)
+        sampler = RandomReshufflingSampler(data)
         
         n_epochs = 3
         indices = []
-        for i_epoch in range(n_epochs):
+        for _ in range(n_epochs):
             sampler_it = iter(sampler)
             idx = []
             for i in range(len(data)):
@@ -26,3 +26,26 @@ class ShufflingTorchTest(unittest.TestCase):
             if idx_1 is not idx_2:
                 self.assertNotEqual(idx_1, idx_2)
                 self.assertEqual(set(idx_1), set(idx_2))
+
+
+class ShuffleOnceTorchTest(unittest.TestCase):
+    def test_shuffle_once(self):
+        data = np.arange(10)
+        sampler = ShuffleOnceSampler(data)
+        
+        n_epochs = 3
+        indices = []
+        for _ in range(n_epochs):
+            sampler_it = iter(sampler)
+            idx = []
+            for i in range(len(data)):
+                el = next(sampler_it)
+                idx.append(el)
+            indices.append(idx)
+            self.assertEqual(len(idx), len(data))
+
+        for idx_1, idx_2 in product(indices, indices):
+            if idx_1 is not idx_2:
+                self.assertTrue(any(idx_1 != data))
+                self.assertTrue(any(idx_2 != data))
+                self.assertEqual(idx_1, idx_2)
