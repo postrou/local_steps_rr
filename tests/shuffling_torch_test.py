8cb9bfedd4404d4fd5894ca4e743315687599e00
Petr Ostroukhov <ostroukhov@phystech.edu>
2024-10-09 13:40:05 +0300

    shuffling_torch_test.py: added tests for seeds for shuffle once case

--- tests/shuffling_torch_test.py
+++ tests/shuffling_torch_test.py
@@ -49,3 +49,54 @@ class ShuffleOnceTorchTest(unittest.TestCase):
                 self.assertTrue(any(idx_1 != data))
                 self.assertTrue(any(idx_2 != data))
                 self.assertEqual(idx_1, idx_2)
+
+    def test_same_seeds(self):
+        data = np.arange(10)
+        
+        n_epochs = 3
+        seed_idx = []
+        for seed in [0, 0]:
+            np.random.seed(seed)
+            sampler = ShuffleOnceSampler(data)
+            indices = []
+            seed_idx.append(indices)
+            for _ in range(n_epochs):
+                sampler_it = iter(sampler)
+                idx = []
+                indices.append(idx)
+                for i in range(len(data)):
+                    el = next(sampler_it)
+                    idx.append(el)
+                self.assertEqual(len(idx), len(data))
+
+        for idx_1, idx_2 in product(seed_idx[0], seed_idx[1]):
+            if idx_1 is not idx_2:
+                self.assertTrue(any(idx_1 != data))
+                self.assertTrue(any(idx_2 != data))
+                self.assertEqual(idx_1, idx_2)
+
+
+    def test_different_seeds(self):
+        data = np.arange(10)
+        
+        n_epochs = 3
+        seed_idx = []
+        for seed in range(2):
+            np.random.seed(seed)
+            sampler = ShuffleOnceSampler(data)
+            indices = []
+            seed_idx.append(indices)
+            for _ in range(n_epochs):
+                sampler_it = iter(sampler)
+                idx = []
+                indices.append(idx)
+                for i in range(len(data)):
+                    el = next(sampler_it)
+                    idx.append(el)
+                self.assertEqual(len(idx), len(data))
+
+        for idx_1, idx_2 in product(seed_idx[0], seed_idx[1]):
+            if idx_1 is not idx_2:
+                self.assertTrue(any(idx_1 != data))
+                self.assertTrue(any(idx_2 != data))
+                self.assertNotEqual(idx_1, idx_2)
